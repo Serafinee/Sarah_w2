8cd196d598d7a4c944b7e5e858c58f0f4260f91b
Sarah <sarah.klausen@inn.no>
2025-09-29 20:45:22 +0200

    How to move things to subfolders using commands

---
+++ Git_toturial.R
@@ -0,0 +1,27 @@
+# Linking a new R project with remote repository in Github
+#Write this in the terminal:
+
+git init #Initializes git in your R project
+git add . # stages all the files in your project
+git commit -m "Message for commit" #Commits the staged files to your Git repository. Always include a descriptive messageregarding your change
+
+#Connecting to the remote repository
+git remote add origin <URL to GitHub repository>
+
+git branch -M main  # Rename the branch to 'main' (if not already named 'main')
+git push -u origin main # set main branch in remote repository
+
+git rm -r --cached <name> # Stop tracking a file in the online repository (aka remove it from there)
+  
+#When starting your workflow, connect to Git hub by writing:
+git pull #Pulls any upates from git Hub to your local computer
+#Then you can work with whatever you need on the computer and finally do this:
+git add . #Stages all files that have either been added or modified.
+git commit -m "commit mesage" #Commits your stages
+git push # push all your commits to Git Hub
+
+
+#Staging
+git add . # Adds all new files. "." refers to the main directory
+git add -u # adds all files that are already tracked, but ignores newly adde files
+gitt add *.R #Adds al files that are of a specific type, for example .R or .qmd etc.
