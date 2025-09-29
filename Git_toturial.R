19a8599c56430ee097958be2e67daa194029aca9
Sarah <sarah.klausen@inn.no>
2025-09-29 20:17:57 +0200

    Added pull, stage, commit and push workflow commands to Git_toturial.R

--- Git_toturial.R
+++ Git_toturial.R
@@ -9,5 +9,13 @@ git commit -m "Message for commit" #Commits the staged files to your Git reposit
 git remote add origin <URL to GitHub repository>
 
 git branch -M main  # Rename the branch to 'main' (if not already named 'main')
-git push -u origin main
+git push -u origin main # set main branch in remote repository
 
+git rm -r --cached <name> # Stop tracking a file in the online repository (aka remove it from there)
+  
+#When starting your workflow, connect to Git hub by writing:
+git pull #Pulls any upates from git Hub to your local computer
+#Then you can work with whatever you need on the computer and finally do this:
+git add . #Stages all files that have either been added or modified.
+git commit -m "commit mesage" #Commits your stages
+git push # push all your commits to Git Hub
